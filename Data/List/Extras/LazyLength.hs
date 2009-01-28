9102a2706fc80eed0cf62cd421cc06f9e31afaaf
wren gayle romano <wren@community.haskell.org>
2009-01-28 17:32:20 -0500

    Data.List.Extras.LazyLength: made package names italic in documentation
    darcs-hash:9ee7ff65d6c1a88078d16910a44da4d0f0e4669d

--- Data/List/Extras/LazyLength.hs
+++ Data/List/Extras/LazyLength.hs
@@ -42,8 +42,8 @@
 -- functions with the same lazy effect as if we used Peano integers,
 -- but does so efficiently instead.
 --
--- (For Peano integers see numbers:"Data.Number.Natural" or
--- non-negative:"Numeric.NonNegative.Class".)
+-- (For Peano integers see /numbers:/"Data.Number.Natural" or
+-- /non-negative:/"Numeric.NonNegative.Class".)
 ----------------------------------------------------------------
 
 module Data.List.Extras.LazyLength
