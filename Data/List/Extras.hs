6258fee25e0d7a6ec311e5f3023351ada8b4135a
wren gayle romano <wren@community.haskell.org>
2010-04-06 00:46:24 -0400

    Data.List.Extras: added function @list@ by Tom Lokhorst
    darcs-hash:7b96d2ed97f086ea0d561c4eb3cf2d9c0d51cac0

--- Data/List/Extras.hs
+++ Data/List/Extras.hs
@@ -2,20 +2,23 @@
 {-# OPTIONS_GHC -Wall -fwarn-tabs #-}
 
 ----------------------------------------------------------------
---                                                  ~ 2008.07.25
+--                                                  ~ 2010.04.05
 -- |
 -- Module      :  Data.List.Extras
--- Copyright   :  Copyright (c) 2007--2009 wren ng thornton
+-- Copyright   :  Copyright (c) 2007--2010 wren ng thornton
 -- License     :  BSD3
 -- Maintainer  :  wren@community.haskell.org
 -- Stability   :  stable
 -- Portability :  portable
 --
--- This module provides a single header for all @Data.List.Extras.*@ modules
+-- This module provides a single header for all @Data.List.Extras.*@
+-- modules and provides a small number of other utility functions.
 ----------------------------------------------------------------
 
 module Data.List.Extras
-    ( module Data.List.Extras.LazyLength
+    (
+      list
+    , module Data.List.Extras.LazyLength
     , module Data.List.Extras.Pair
     , module Data.List.Extras.Argmax
     ) where
@@ -23,5 +26,14 @@ module Data.List.Extras
 import Data.List.Extras.LazyLength
 import Data.List.Extras.Pair
 import Data.List.Extras.Argmax
+
+-- | Pattern matching for lists, as a first-class function. (Could
+-- also be considered as a non-recursive 'foldr'.) If the list
+-- argument is @[]@ then the default argument is returned; otherwise
+-- the function is called with the head and tail of the list.
+list :: (a -> [a] -> b) -> b -> [a] -> b
+list _ z []     = z
+list f _ (x:xs) = f x xs 
+
 ----------------------------------------------------------------
 ----------------------------------------------------------- fin.
